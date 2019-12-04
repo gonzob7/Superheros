6d846a9bd711bbede8f1032e5eb7300295e3e7af
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-03 17:45:34 -0800

    started ability class

---
+++ superheroes.py
@@ -0,0 +1,13 @@
+import random
+
+class Ability:
+    def __init__(self, name, attack_strength):
+        '''
+       Initialize the values passed into this
+       method as instance variables.
+        '''
+
+        # Assign the "name" and "max_damage"
+        # for a specific instance of the Ability class
+        self.name = name
+        self.max_damage = max_damage
