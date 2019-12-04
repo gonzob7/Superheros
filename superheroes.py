d88efdec93d9c9889217f7650bdad0ef20ff0d42
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-04 13:34:28 -0800

    added remove hero method to Team class

--- superheroes.py
+++ superheroes.py
@@ -154,6 +154,24 @@ class Team:
         self.name = name
         self.heroes = list()
 
+    def remove_hero(self, name):
+        '''Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        foundHero = False
+        # loop through each hero in our list
+        for hero in self.heroes:
+            # if we find them, remove them from the list
+            if hero.name == name:
+                self.heroes.remove(hero)
+                # set our indicator to True
+                foundHero = True
+        # if we looped through our list and did not find our hero,
+        # the indicator would have never changed, so return 0
+        if not foundHero:
+            return 0
+
+
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
