abd3d22503b4e9e556c4209a5666257f6ef86085
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-03 17:47:46 -0800

    added main block for testing and running

--- superheroes.py
+++ superheroes.py
@@ -18,3 +18,14 @@ class Ability:
           # Pick a random value between 0 and self.max_damage
           random_value = random.randint(0,self.max_damage)
           return random_value
+
+
+
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
