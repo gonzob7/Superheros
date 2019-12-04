c2fc66b8bb6e1e84435c8247f778784d80dc4fd5
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-03 18:25:50 -0800

    started Hero class

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,29 @@
 import random
 
+class Hero:
+    # We want our hero to have a default "starting_health",
+    # so we can set that in the function header.
+    def __init__(self, name, starting_health=100):
+      '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+      '''
+
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
+        # we know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
+        self.starting_health = starting_health
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
+
 class Ability:
     def __init__(self, name, attack_strength):
         '''
