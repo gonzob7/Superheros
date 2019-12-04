10bd9b9694415ff0bbf33f4ca9632694fba61c3a
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-03 18:48:07 -0800

    added add ability method to hero

--- superheroes.py
+++ superheroes.py
@@ -4,14 +4,6 @@ class Hero:
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
-      '''Instance properties:
-          abilities: List
-          armors: List
-          name: String
-          starting_health: Integer
-          current_health: Integer
-      '''
-
         # abilities and armors don't have starting values,
         # and are set to empty lists on initialization
         self.abilities = list()
@@ -23,6 +15,17 @@ class Hero:
         # when a hero is created, their current health is
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
+
+
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+
+        # We used the append method to add strings to a list
+        # in the Rainbow Checklist tutorial. This time,
+        # we're not adding strings, instead we'll add ability objects.
+        self.abilities.append(ability)
+
+
 
 class Ability:
     def __init__(self, name, attack_strength):
@@ -69,6 +72,7 @@ class Armor:
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    armor = Armor("Debugging Shield", 10)
-    print(armor.name)
-    print(armor.block())
+    ability = Ability("Great Debugging", 50)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    print(hero.abilities)
